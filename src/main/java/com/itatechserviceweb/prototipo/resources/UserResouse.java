154b2cb8b27ea455943b28abc9d6ff1261d3e897
Itatelino de Oliveira Leite Neto <itatelino.oliveira@gmail.com>
2023-10-24 15:16:17 -0300

    User entity and resource

---
+++ src/main/java/com/itatechserviceweb/prototipo/resources/UserResouse.java
@@ -0,0 +1,21 @@
+package com.itatechserviceweb.prototipo.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itatechserviceweb.prototipo.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResouse {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+
+}
