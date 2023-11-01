647acd8c11adb728b98de84cc7116ed68f905240
Itatelino de Oliveira Leite Neto <itatelino.oliveira@gmail.com>
2023-11-01 10:47:50 -0300

    Entidade pedido. Datas com Instant e padrao ISO 8601

---
+++ src/main/java/com/itatechserviceweb/prototipo/services/UserService.java
@@ -0,0 +1,27 @@
+package com.itatechserviceweb.prototipo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itatechserviceweb.prototipo.entities.User;
+import com.itatechserviceweb.prototipo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
