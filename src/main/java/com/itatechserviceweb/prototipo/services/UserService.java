4eabaaffda880dcc2b0b0ca82add8dcc17d9935d
Itatelino de Oliveira Leite Neto <itatelino.oliveira@gmail.com>
2023-11-13 16:58:39 -0300

    Tratamento de excecao - findById

--- src/main/java/com/itatechserviceweb/prototipo/services/UserService.java
+++ src/main/java/com/itatechserviceweb/prototipo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.itatechserviceweb.prototipo.entities.User;
 import com.itatechserviceweb.prototipo.repositories.UserRepository;
+import com.itatechserviceweb.prototipo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()  -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
